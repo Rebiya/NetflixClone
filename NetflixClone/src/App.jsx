7e513cc9cb50a808cb5863a5552a74a21bd1affd
rebu <rebum.19@gmail.com>
2024-11-24 01:05:08 +0300

    The Row component has been finished

--- NetflixClone/src/App.jsx
+++ NetflixClone/src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./Pages/Home/Home";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
       <Home />
+      <ToastContainer />
     </>
   );
 }
